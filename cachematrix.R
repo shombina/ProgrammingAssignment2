8b7553abbc799e960fa0a51f601df7831cf4cbea
shombina <shombina@gmail.com>
2014-09-22 02:32:02 +0400

    Yet another submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
 # The result of matrix inversion is being cached during the first attempt
 # and then is stored and returned if needed withot being computed again.
 
